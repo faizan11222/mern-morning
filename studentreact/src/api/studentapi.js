545de46e561c565ca12890d40f21bda949f933e2
faizan <62203593+faizan11222@users.noreply.github.com>
2026-04-27 10:32:49 +0500

    react setup

---
+++ studentreact/src/api/studentapi.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+//creating a function to get backend URL 
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    //also need to tell the frontend that our APIs are in JSON format
+    headers: {'Content-Type':'application/json'}
+})
