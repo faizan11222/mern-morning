a3eccace4876db8a9976dbe5707aa723d35c2083
faizan <62203593+faizan11222@users.noreply.github.com>
2026-04-28 09:44:44 +0500

    all apis

--- studentreact/src/api/studentapi.js
+++ studentreact/src/api/studentapi.js
@@ -8,4 +8,12 @@ const api = axios.create({
 })
 
 //function to get allstudents API
-export const getAllStudents = async() => (await api.get('/students/allstudents'))
+export const getAllStudents = async() => (await api.get('/api/students/allstudents')).data
+//function to get student by ID API
+export const getStudentById = async(id) => (await api.get(`/api/students/getstudent/${id}`)).data
+//function to add student API
+export const addStudent = async(data) => (await api.post('/api/students/addstudents/',data)).data
+//function to update student API
+export const updateStudent = async(id,data) => (await api.put(`/api/students/update/${id}`,data)).data
+//function to delete student API
+export const deleteStudent = async(id) => (await api.delete(`/api/students/delete/${id}`)).data
