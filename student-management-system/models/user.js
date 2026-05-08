20b78148c5a5ddb4e444b0f4ae58c5f1b4ab7624
faizan <62203593+faizan11222@users.noreply.github.com>
2026-05-08 10:04:34 +0500

    schema completed

--- student-management-system/models/user.js
+++ student-management-system/models/user.js
@@ -28,4 +28,19 @@ const UserSchema = new mongoose.Schema({
     }
 },{timestamps:true})
 
+/*if we are updating the fields (name or email) then password
+remain hash only one time
+making my password field hash/encrypt
+*/
+UserSchema.pre('save',async function (){
+    if(!this.isModified('password')) return
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password,salt)
+})
+
+//entered password vs hash password
+UserSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password)
+}
 module.exports = mongoose.model('User',UserSchema)
